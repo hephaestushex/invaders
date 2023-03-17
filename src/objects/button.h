732cea224b21f1e801b88945252a3862bd409fcb
hephaestushex <81438978+hephaestushex@users.noreply.github.com~>
2023-03-17 11:57:12 -0600

    Reworked button click function

--- src/objects/button.h
+++ src/objects/button.h
@@ -8,5 +8,10 @@ typedef struct button
 
 //Function to check if the button has been clicked
 
-
-
+bool buttonClicked(Rectangle button)
+{
+    if (IsMouseButtonDown(MOUSE_BUTTON_LEFT) && GetMousePosition().x  >= button.x && GetMousePosition().x  <= button.x + button.width && GetMousePosition().y  >= button.y && GetMousePosition().y  >= button.y + button.height)
+    {
+        return true;
+    }
+}
